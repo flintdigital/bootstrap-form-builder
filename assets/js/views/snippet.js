2d44164192d2831930f78346cc5412c6bc33e630
7ictor <victor@rodriguezv.com>
2015-10-08 09:35:12 -0600

    cleanup rest of views

--- assets/js/views/snippet.js
+++ assets/js/views/snippet.js
@@ -1,56 +1,54 @@
 define([
-  "jquery", "underscore", "backbone"
-  , "text!templates/popover/popover-main.html"
-  , "text!templates/popover/popover-input.html"
-  , "text!templates/popover/popover-select.html"
-  , "text!templates/popover/popover-textarea.html"
-  , "text!templates/popover/popover-textarea-split.html"
-  , "text!templates/popover/popover-checkbox.html"
-  , "templates/snippet/snippet-templates"
-  , "bootstrap"
-], function(
-  $, _, Backbone
-  , _PopoverMain
-  , _PopoverInput
-  , _PopoverSelect
-  , _PopoverTextArea
-  , _PopoverTextAreaSplit
-  , _PopoverCheckbox
-  , _snippetTemplates
-){
+  'backbone',
+  'text!templates/popover/popover-main.html',
+  'text!templates/popover/popover-input.html',
+  'text!templates/popover/popover-select.html',
+  'text!templates/popover/popover-textarea.html',
+  'text!templates/popover/popover-textarea-split.html',
+  'text!templates/popover/popover-checkbox.html',
+  'templates/snippet/snippet-templates',
+  'bootstrap'
+],
+
+function (Backbone, _PopoverMain, _PopoverInput, _PopoverSelect,
+          _PopoverTextArea, _PopoverTextAreaSplit, _PopoverCheckbox,
+          _snippetTemplates) {
+
   return Backbone.View.extend({
-    tagName: "div"
-    , className: "component" 
-    , initialize: function(){
-      this.template = _.template(_snippetTemplates[this.model.idFriendlyTitle()])
+    tagName: 'div',
+    className: 'component',
+    initialize: function () {
+      this.template = _.template(_snippetTemplates[this.model.idFriendlyTitle()]);
       this.popoverTemplates = {
-        "input" : _.template(_PopoverInput)
-        , "select" : _.template(_PopoverSelect)
-        , "textarea" : _.template(_PopoverTextArea)
-        , "textarea-split" : _.template(_PopoverTextAreaSplit)
-        , "checkbox" : _.template(_PopoverCheckbox)
-      }
-    }
-    , render: function(withAttributes){
-      var that = this;
+        'input' : _.template(_PopoverInput),
+        'select' : _.template(_PopoverSelect),
+        'textarea' : _.template(_PopoverTextArea),
+        'textarea-split' : _.template(_PopoverTextAreaSplit),
+        'checkbox' : _.template(_PopoverCheckbox)
+      };
+    },
+
+    render: function (withAttributes) {
+      var self = this;
       var content = _.template(_PopoverMain)({
-        "title": that.model.get("title"),
-        "items" : that.model.get("fields"),
-        "popoverTemplates": that.popoverTemplates
+        'title': self.model.get('title'),
+        'items' : self.model.get('fields'),
+        'popoverTemplates': self.popoverTemplates
       });
+
       if (withAttributes) {
         return this.$el.html(
-          that.template(that.model.getValues())
+          self.template(self.model.getValues())
         ).attr({
-          "data-content"     : content
-          , "data-title"     : that.model.get("title")
-          , "data-trigger"   : "manual"
-          , "data-html"      : true
+          'data-content'     : content,
+          'data-title'     : self.model.get('title'),
+          'data-trigger'   : 'manual',
+          'data-html'      : true
         });
       } else {
         return this.$el.html(
-          that.template(that.model.getValues())
-        )
+          self.template(self.model.getValues())
+        );
       }
     }
   });
