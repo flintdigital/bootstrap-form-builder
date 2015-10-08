98d2bd962302889baaee74951cbdac3a352919f1
7ictor <victor@rodriguezv.com>
2015-10-07 20:04:07 -0600

    cleaned tab and my-form views

--- assets/js/views/my-form.js
+++ assets/js/views/my-form.js
@@ -1,91 +1,102 @@
 define([
-       "jquery", "underscore", "backbone"
-      , "views/temp-snippet"
-      , "helper/pubsub"
-      , "text!templates/app/renderform.html"
-], function(
-  $, _, Backbone
-  , TempSnippetView
-  , PubSub
-  , _renderForm
-){
+  'backbone',
+  'views/temp-snippet',
+  'helper/pubsub',
+  'text!templates/app/renderform.html'
+],
+
+function (Backbone, TempSnippetView, PubSub, _renderForm) {
+
   return Backbone.View.extend({
-    tagName: "fieldset"
-    , initialize: function(){
-      this.collection.on("add", this.render, this);
-      this.collection.on("remove", this.render, this);
-      this.collection.on("change", this.render, this);
-      PubSub.on("mySnippetDrag", this.handleSnippetDrag, this);
-      PubSub.on("tempMove", this.handleTempMove, this);
-      PubSub.on("tempDrop", this.handleTempDrop, this);
-      this.$build = $("#build");
-      this.build = document.getElementById("build");
+
+    tagName: 'fieldset',
+
+    initialize: function () {
+      this.collection.on('add', this.render, this);
+      this.collection.on('remove', this.render, this);
+      this.collection.on('change', this.render, this);
+
+      PubSub.on('mySnippetDrag', this.handleSnippetDrag, this);
+      PubSub.on('tempMove', this.handleTempMove, this);
+      PubSub.on('tempDrop', this.handleTempDrop, this);
+
+      this.build = document.getElementById('build')
       this.buildBCR = this.build.getBoundingClientRect();
       this.renderForm = _.template(_renderForm);
       this.render();
-    }
+    },
 
-    , render: function(){
+    render: function () {
       //Render Snippet Views
-      this.$el.empty();
-      var that = this;
+      var self = this;
+      self.$el.empty();
       var containsFile = false;
-      _.each(this.collection.renderAll(), function(snippet){
-        that.$el.append(snippet);
-      });
-      $("#render").val(that.renderForm({
-        multipart: this.collection.containsFileType(),
-        text: _.map(this.collection.renderAllClean(), function(e){return e.html()}).join("\n")
-      }));
-      this.$el.appendTo("#build form");
-      this.delegateEvents();
-    }
 
-    , getBottomAbove: function(eventY){
-      var myFormBits = $(this.$el.find(".component"));
-      var topelement = _.find(myFormBits, function(renderedSnippet) {
-        if (($(renderedSnippet).position().top + $(renderedSnippet).height()) > eventY  - 160) {
-          return true;
-        }
-        else {
-          return false;
-        }
+      _.each(self.collection.renderAll(), function (snippet) {
+        self.$el.append(snippet);
       });
-      if (topelement){
+
+      $('#render').val(self.renderForm({
+        multipart: self.collection.containsFileType(),
+        text: _.map(self.collection.renderAllClean(), function (e) {
+          return e.html()
+        }).join('\n')
+      }));
+
+      self.$el.appendTo('#build form');
+      self.delegateEvents();
+    },
+
+    getBottomAbove: function (eventY) {
+      var myFormBits = $(this.$el.find('.component'));
+      var topelement = _.find(myFormBits, function (renderedSnippet) {
+        var snippetY = $(renderedSnippet).position().top +
+                       $(renderedSnippet).height();
+        if (snippetY > eventY - 160) return true;
+        else return false;
+      });
+
+      if (topelement) {
         return topelement;
       } else {
         return myFormBits[0];
       }
-    }
+    },
 
-    , handleSnippetDrag: function(mouseEvent, snippetModel) {
-      $("body").append(new TempSnippetView({model: snippetModel}).render());
+    handleSnippetDrag: function (mouseEvent, snippetModel) {
+      $('body').append(new TempSnippetView({model: snippetModel}).render());
       this.collection.remove(snippetModel);
-      PubSub.trigger("newTempPostRender", mouseEvent);
-    }
+      PubSub.trigger('newTempPostRender', mouseEvent);
+    },
 
-    , handleTempMove: function(mouseEvent){
-      $(".target").removeClass("target");
-      if(mouseEvent.pageX >= this.buildBCR.left &&
-          mouseEvent.pageX < (this.$build.width() + this.buildBCR.left) &&
+    handleTempMove: function (mouseEvent) {
+      $('.target').removeClass('target');
+      var width = this.build.offsetWidth;
+      var height = this.build.offsetHeight;
+      if (mouseEvent.pageX >= this.buildBCR.left &&
+          mouseEvent.pageX <= (width + this.buildBCR.left) &&
           mouseEvent.pageY >= this.buildBCR.top &&
-          mouseEvent.pageY < (this.$build.height() + this.buildBCR.top)){
-        $(this.getBottomAbove(mouseEvent.pageY)).addClass("target");
-      } else {
-        $(".target").removeClass("target");
-      }
-    }
+          mouseEvent.pageY <= (height + this.buildBCR.top)) {
 
-    , handleTempDrop: function(mouseEvent, model, index){
-      if(mouseEvent.pageX >= this.buildBCR.left &&
-         mouseEvent.pageX < (this.$build.width() + this.buildBCR.left) &&
-         mouseEvent.pageY >= this.buildBCR.top &&
-         mouseEvent.pageY < (this.$build.height() + this.buildBCR.top)) {
-        var index = $(".target").index();
-        $(".target").removeClass("target");
-        this.collection.add(model,{at: index+1});
+        $(this.getBottomAbove(mouseEvent.pageY)).addClass('target');
       } else {
-        $(".target").removeClass("target");
+        $('.target').removeClass('target');
+      }
+    },
+
+    handleTempDrop: function (mouseEvent, model, index) {
+      var width = this.build.offsetWidth;
+      var height = this.build.offsetHeight;
+      if (mouseEvent.pageX >= this.buildBCR.left &&
+         mouseEvent.pageX <= (width + this.buildBCR.left) &&
+         mouseEvent.pageY >= this.buildBCR.top &&
+         mouseEvent.pageY <= (height + this.buildBCR.top)) {
+
+        var index = $('.target').index();
+        $('.target').removeClass('target');
+        this.collection.add(model,{at: index + 1});
+      } else {
+        $('.target').removeClass('target');
       }
     }
   })
--- assets/js/views/tab.js
+++ assets/js/views/tab.js
@@ -19,20 +19,23 @@ function (Backbone, _tabNavTemplate) {
 
     render: function () {
       // Render Snippet Views
-      var that = this;
-      if (that.collection !== undefined) {
-        _.each(this.collection.renderAll(), function(snippet){
-          that.$el.append(snippet);
+      var self = this;
+      if (self.collection !== undefined) {
+        _.each(this.collection.renderAll(), function (snippet) {
+          self.$el.append(snippet);
         });
-      } else if (that.options.content){
-        that.$el.append(that.options.content);
+      } else if (self.options.content) {
+        self.$el.append(self.options.content);
       }
       // Render & append nav for tab
-      $("#formtabs").append(this.tabNavTemplate({title: this.options.title, id: this.id}))
+      $('#formtabs').append(self.tabNavTemplate({
+        title: self.options.title,
+        id: self.id
+      }));
       // Render tab
-      this.$el.attr("id", this.id);
-      this.$el.appendTo(".tab-content");
-      this.delegateEvents();
+      self.$el.attr('id', self.id);
+      self.$el.appendTo('.tab-content');
+      self.delegateEvents();
     }
   });
 });
